513e59c03c177cc54709b49fa1093b75c5f0a3b4
Melvin Kempe <k3mp314@gmail.com>
2023-04-18 11:46:50 +0200

    added an input and container to chat

--- frontend/chat.js
+++ frontend/chat.js
@@ -1,14 +1,20 @@
+const chatContainer = document.createElement("div");
 const showChatBtn = document.createElement("button");
 const chatInput = document.createElement("input");
 
 export default function init() {
     showChatBtn.innerText = "Chat";
 
-    document.body.appendChild(showChatBtn)
+    document.body.appendChild(chatContainer)
+    chatContainer.appendChild(showChatBtn)
     
     showChatBtn.addEventListener("click", createChat)
 }
 
 function createChat() {
+    const chatInput = document.createElement("input");
 
+    chatInput.placeholder = "Message";
+
+    chatContainer.appendChild(chatInput);
 }
