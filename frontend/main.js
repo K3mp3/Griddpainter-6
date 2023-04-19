53fffa573582a6f72fc991c93ed3fb4a23dbc6a0
Jenny Svensson <jenny.svensson@medieinstitutet.se>
2023-04-19 09:48:26 +0200

    create a table for painting and style of chat

--- frontend/main.js
+++ frontend/main.js
@@ -8,9 +8,32 @@ let messages = document.getElementById("messages");
 socket.on("chat", (arg) => {
     console.log("chat", arg);
 
-    messages.innerHTML += arg.chat + " | from: " + arg.user + "<br/>";
+    messages.innerHTML += "<li>" + arg.chat + " ---- from: " + arg.user + "</li>";
 })
 
 sendBtn.addEventListener("click", () => {
-    socket.emit("chat", {chat: sendMessage.value, user: "Janne"});
+    socket.emit("chat", {chat: sendMessage.value, user: "User 1"});
 })
+
+
+const table = document.createElement('table');
+for (let i = 1; i <= 15; i++) {
+    const row = document.createElement('tr');
+    for (let r = 1; r <= 15; r++) {
+        const cell = document.createElement('td');
+        let cellID = `cell_${i}_${r}`;
+        cell.id = cellID;
+        cell.addEventListener('click', () => {
+            console.log(cellID);
+            cell.classList.add('red');  // add class "red" to the clicked cell (change later to the color that user pick)
+
+        });
+        row.appendChild(cell);
+    }
+    table.appendChild(row);
+}
+document.body.appendChild(table);
+
+const saveBtn = document.createElement('button');
+saveBtn.innerText = "Save";
+document.body.appendChild(saveBtn);
