476df167857d0d055d5596846c2d8eee2ad1a0c9
Melvin Kempe <k3mp314@gmail.com>
2023-04-19 09:41:53 +0200

    added new JS code to implement a new chat design

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,50 @@
 import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
 const socket = io("http://localhost:3000");
 
-let sendMessage = document.getElementById("sendMessage");
-let sendBtn = document.getElementById("sendBtn");
-let messages = document.getElementById("messages");
+const chatContainer = document.createElement("div");
+const showChatBtn = document.createElement("button");
+const chatInput = document.createElement("input");
 
-socket.on("chat", (arg) => {
-    console.log("chat", arg);
+export default function init() {
+    showChatBtn.innerText = "Chat";
 
-    messages.innerHTML += arg.chat + " | from: " + arg.user + "<br/>";
-})
+    document.body.appendChild(chatContainer)
+    chatContainer.appendChild(showChatBtn)
+    
+    showChatBtn.addEventListener("click", createChat)
+}
 
-sendBtn.addEventListener("click", () => {
-    socket.emit("chat", {chat: sendMessage.value, user: "Janne"});
-})
+function createChat() {
+    const chatInput = document.createElement("input");
+    const sendChatBtn = document.createElement("button");
+
+    chatInput.classList.add("chat-input");
+
+    chatInput.placeholder = "Message";
+    sendChatBtn.innerText = "Send chat";
+
+
+    chatContainer.appendChild(chatInput);
+    chatContainer.appendChild(sendChatBtn);
+
+    sendChatBtn.addEventListener("click", sendChat);
+}
+
+function sendChat(e) {
+    const chatInput = document.querySelector(".chat-input");
+
+    const chatMsg = chatInput.value;
+    console.log("chatMsg2", chatMsg);
+
+    socket.emit("chat",{chat: chatMsg});
+
+    getChatMsg();
+}
+
+function getChatMsg() {
+    /*const chat = document.createElement("p");
+    chatContainer.appendChild(chat);
+    socket.on("chat message", function(msg) {
+        console.log("msg", msg);
+    })*/
+}
